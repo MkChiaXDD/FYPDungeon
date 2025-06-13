4cae40aeda2d92fd130785ecf4b0513988be7ee1
MkChiaXDD <122813605+MkChiaXDD@users.noreply.github.com>
2025-06-13 15:51:09 +0800

    Revert "All baisc enemies implemented"
    This reverts commit ca73cf6895cae6392bb2e8a57adf4d2b3c942270.

--- Assets/MengKiat/Scripts/Enemies/EnemyData.cs
+++ Assets/MengKiat/Scripts/Enemies/EnemyData.cs
@@ -7,10 +7,6 @@ public class EnemyData : ScriptableObject
     public string enemyName;
     public int maxHealth;
     public float moveSpeed;
-<<<<<<< Updated upstream
     public int damage;
     public GameObject prefab;
-=======
-    public float damage;
->>>>>>> Stashed changes
 }
