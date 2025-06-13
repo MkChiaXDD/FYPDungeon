461cec04cb3642ab8d9a1036502bc596c6881417
Sorbetisbadatart <155771636+Sorbetisbadatart@users.noreply.github.com>
2025-06-13 14:21:44 +0800

    added IDamagable

--- Assets/MengKiat/Scripts/Enemies/IDamageable.cs
+++ Assets/MengKiat/Scripts/Enemies/IDamageable.cs
@@ -1,5 +1,54 @@
-// IDamageable.cs
-public interface IDamageable
+using System.Collections;
+using UnityEngine;
+
+public class IDamageable : MonoBehaviour
 {
-    void TakeDamage(float amt);
+    private float health = 100f;
+
+
+    //create hit effect anim
+    private Color originalColour;
+    private Color damageColour = Color.red;
+    private float damageDuration = 0.5f;
+    private Renderer _renderer;
+
+    private void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+        originalColour = _renderer.material.color;
+    }
+
+    private IEnumerator DamageEffect()
+    {
+        _renderer.material.color = damageColour;
+        float elapseTime = 0.0f;
+        while (elapseTime <= damageDuration)
+        {
+            _renderer.material.color = Color.Lerp(damageColour, originalColour, elapseTime / damageDuration);
+            elapseTime += Time.deltaTime;
+            yield return null;
+        }
+        _renderer.material.color = originalColour;
+    }
+
+    public void TakeDamage(float amount)
+    {
+
+        if (_renderer != null)
+        {
+            StartCoroutine(DamageEffect());
+        }
+        Debug.Log("ouch");
+        health -= amount;
+        if (health <= 0)
+        {
+            Debug.Log("health: " + health);
+            Destroy(gameObject);
+
+        }
+    }
+
 }
+
+
+
