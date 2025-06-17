6f778a1dc8c96ab6725938f88f22e37b7678fe76
Sorbetisbadatart <155771636+Sorbetisbadatart@users.noreply.github.com>
2025-06-17 13:10:20 +0800

    normal sword attack

--- Assets/Matthew/Scripts/Spells/EarthShatterAttack.cs
+++ Assets/Matthew/Scripts/Spells/EarthShatterAttack.cs
@@ -8,7 +8,7 @@ public class EarthShatterAttack : MonoBehaviour
     // New serialized fields for damage system
     [SerializeField] private float damageRadius = 5f;
     [SerializeField] private int damageAmount = 10;
-    [SerializeField] private LayerMask damageableLayers;
+   // [SerializeField] private LayerMask damageableLayers;
 
     // New function to check for IDamageable objects
     private void OnTriggerEnter(Collider other)
