3a1cdfa40267b4ff59642345d65abd2000be228d
MkChiaXDD <122813605+MkChiaXDD@users.noreply.github.com>
2025-06-19 11:15:24 +0800

    lol

---
+++ Assets/MengKiat/Scripts/Traps/SpikeTrap.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrap : MonoBehaviour
+{
+    [SerializeField] private int damage = 2;
+    [SerializeField] private float activateDuration = 3f;
+    [SerializeField] private float activeDuration = 1f;
+    [SerializeField] private float knockbackForce = 10f;
+    private float timer;
+    private bool isActivated = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (!isActivated)
+        {
+            if (timer > activateDuration)
+            {
+                Debug.Log("SPIKETRAP: TRAP ACTIVATED!");
+                isActivated = true;
+                timer = 0;
+            }
+        }
+        else
+        {
+            if (timer > activeDuration)
+            {
+                Debug.Log("SPIKETRAP: TRAP DEACTIVATED!");
+                isActivated = false;
+                timer = 0;
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!isActivated) return;
+
+        if (other.CompareTag("Player"))
+        {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                Vector3 knockbackDir = (other.transform.position - transform.position).normalized;
+                knockbackDir.y = 0f; // Optional: prevent vertical launch
+                rb.AddForce(knockbackDir * knockbackForce, ForceMode.Impulse);
+
+                if (other.TryGetComponent(out IDamageable damageable))
+                {
+                    damageable.TakeDamage(damage);
+                    Debug.Log("SPIKETRAP: HIT Something");
+                }
+            }
+
+            Debug.Log("SPIKETRAP: Player took damage and was knocked back!");
+        }
+    }
+}
