232144547fe1d6764b6ce182df28321969b38968
MkChiaXDD <122813605+MkChiaXDD@users.noreply.github.com>
2025-06-24 09:46:35 +0800

    s

---
+++ Assets/MengKiat/RoomStuff/HAHAHA/EnemySpawner.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public List<GameObject> enemyPrefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
