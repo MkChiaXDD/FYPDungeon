e1d703840d01a9fd2148528f89a56c1bf75b88d6
MkChiaXDD <122813605+MkChiaXDD@users.noreply.github.com>
2025-06-24 11:13:51 +0800

    kk

--- Assets/MengKiat/RoomStuff/HAHAHA/EnemySpawner.cs
+++ Assets/MengKiat/RoomStuff/HAHAHA/EnemySpawner.cs
@@ -1,18 +1,49 @@
+using RMG;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public List<GameObject> enemyPrefabs;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private MapGenerator mapGen;
+    [SerializeField] private List<GameObject> enemyPrefabs;
+    [SerializeField] private Transform enemyParent;
+    [SerializeField] private int minAmount = 1;
+    [SerializeField] private int maxAmount = 3;
+    [SerializeField] private float offSet = 1.5f;
 
-    // Update is called once per frame
-    void Update()
+    public void GetAllRoomSpawnPoint()
     {
-        
+        if (mapGen == null || enemyPrefabs == null || enemyPrefabs.Count == 0)
+        {
+            Debug.LogWarning("Spawner is not set up correctly.");
+            return;
+        }
+
+        List<Room> allRooms = mapGen.spawnedRooms;
+
+        foreach (Room room in allRooms)
+        {
+            Transform spawnPoint = room.transform.Find("EnemySpawnPoint");
+
+            if (spawnPoint != null)
+            {
+                int numOfEnemies = Random.Range(minAmount, maxAmount + 1);
+
+                for (int i = 0; i < numOfEnemies; i++)
+                {
+                    Vector3 spawnOffset = new Vector3(Random.Range(-offSet, offSet), 0, Random.Range(-offSet, offSet));
+
+                    Vector3 spawnPos = spawnPoint.position + spawnOffset;
+
+                    GameObject enemy = Instantiate(enemyPrefabs[Random.Range(0, enemyPrefabs.Count)], spawnPos, Quaternion.identity, enemyParent);
+
+                    enemy.transform.parent = room.transform;
+                }
+            }
+            else
+            {
+                Debug.LogWarning("No 'EnemySpawnPoint' found in " + room.name);
+            }
+        }
     }
 }
--- Assets/MengKiat/RoomStuff/Scripts/MapGenerator.cs
+++ Assets/MengKiat/RoomStuff/Scripts/MapGenerator.cs
@@ -38,6 +38,7 @@ namespace RMG
         public int seed { get; private set; }
 
         [SerializeField] private Material farthestRoomMaterial;
+        [SerializeField] private float roomSizeMultiplier = 2;
 
         /// <summary>
         /// Called before Start(). It initializes and categorizes all room prefabs
@@ -134,6 +135,9 @@ namespace RMG
             }
 
             CalculateScores(); // Calculate distance from start for each room
+            transform.localScale = Vector3.one * roomSizeMultiplier;
+            var enemySpawner = FindFirstObjectByType<EnemySpawner>();
+            enemySpawner.GetAllRoomSpawnPoint();
         }
 
         /// <summary>
