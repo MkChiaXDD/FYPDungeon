8f8d378d4cb302534278346cf7ab4fc52105c5d8
Sorbetisbadatart <155771636+Sorbetisbadatart@users.noreply.github.com>
2025-07-08 17:07:07 +0800

    update breakable objects

--- Assets/Matthew/Scripts/Breakable.cs
+++ Assets/Matthew/Scripts/Breakable.cs
@@ -41,7 +41,7 @@ public class Breakable : MonoBehaviour, IDamageable
             }
         }
     }
-    public void TakeElementalDamage(float damage, ElementType element) => Die();
+    public void TakeElementalDamage(float damage, ElementType element) { Debug.Log("no implementation of TakeDamage in breakable currently, it is replaced by physicalDamage"); }
     public void TakeDamage(float damage) { Debug.Log("no implementation of TakeDamage in breakable currently, it is replaced by physicalDamage");  }
     public void TakePhysicalDamage(float damage, PhysicalAttackType attackType)
     {
--- Assets/Matthew/Scripts/Destroy.cs
+++ Assets/Matthew/Scripts/Destroy.cs
@@ -3,15 +3,10 @@ using UnityEngine;
 
 public class Destroy : MonoBehaviour
 {
-
     [SerializeField] private float DestroyCountdownDuration = 5.0f;
-    [SerializeField] private ParticleSystem[] EarthShatterVFX;
-
     // Start is called before the first frame update
     void Start()
-    {
-        foreach (ParticleSystem earthShatterVFX in EarthShatterVFX)
-        earthShatterVFX.Play();
+    { 
         StartCoroutine(DestroySelf());      
     }
     private IEnumerator DestroySelf()
