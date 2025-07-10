01cba3c4c28e73633246be6adaae765bc3057657
Sorbetisbadatart <155771636+Sorbetisbadatart@users.noreply.github.com>
2025-07-10 16:46:29 +0800

    pushing prefabs

--- Assets/MengKiat/Scripts/Others/Portal.cs
+++ Assets/MengKiat/Scripts/Others/Portal.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
+    [SerializeField] private Canvas PortalInteractionCanvas;
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (!PortalInteractionCanvas.isActiveAndEnabled)
+        {
+            PortalInteractionCanvas.gameObject.SetActive(true);
+        }
+
+
+        if (other.CompareTag("PlayerBody"))
         {
             if (Input.GetKeyDown(KeyCode.U))
             {
@@ -14,6 +21,15 @@ public class Portal : MonoBehaviour
                 FindFirstObjectByType<FarthestRoom>().NextLevel();
                 Destroy(gameObject);
             }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (PortalInteractionCanvas.isActiveAndEnabled)
+        {
+            PortalInteractionCanvas.gameObject.SetActive(false);
         }
     }
 }
