95b69efbf2f7453f19d225fd83060ac45c197211
Sorbetisbadatart <155771636+Sorbetisbadatart@users.noreply.github.com>
2025-07-11 11:20:54 +0800

    game now has an ending

---
+++ Assets/Matthew/MainMenu/EndingScene/EndingScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingScript : MonoBehaviour
+{
+
+    [SerializeField] private GameObject EndingPanel;
+
+    public void ProceedToWinscreen()
+    {
+        EndingPanel.SetActive(true);     
+    }
+}
