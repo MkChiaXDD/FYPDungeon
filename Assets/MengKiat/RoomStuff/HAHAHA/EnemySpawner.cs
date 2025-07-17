bc18cacb94b1eb1aac6f10698ecb3de4d7c20dbd
MkChiaXDD <122813605+MkChiaXDD@users.noreply.github.com>
2025-07-17 17:41:58 +0800

    fix shit

--- Assets/MengKiat/RoomStuff/HAHAHA/EnemySpawner.cs
+++ Assets/MengKiat/RoomStuff/HAHAHA/EnemySpawner.cs
@@ -93,7 +93,7 @@ public class EnemySpawner : MonoBehaviour
                     }
 
                     GameObject enemyInstance = Instantiate(chosenEnemy, spawnPos, Quaternion.identity, enemyParent);
-                    enemyInstance.transform.parent = room.transform;
+                    //enemyInstance.transform.parent = room.transform;
                 }
 
             }
