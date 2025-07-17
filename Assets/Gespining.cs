46b7385b209491c7a9ca468037e3e26fc1450430
Sorbetisbadatart <155771636+Sorbetisbadatart@users.noreply.github.com>
2025-07-17 18:06:41 +0800

    refactor

---
+++ Assets/Gespining.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gespining : MonoBehaviour
+{
+    private Vector3 rotation;
+    public float rotationspeed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        rotation.y += rotationspeed;
+       transform.rotation = Quaternion.Euler(rotation); 
+    }
+}
