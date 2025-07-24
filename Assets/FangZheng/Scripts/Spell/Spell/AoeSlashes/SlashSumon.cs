9c1f521644f41d4a794abd98378bb0485ecfca74
bruhifellded <xufangzheng6666@gmail.com>
2025-07-25 04:01:48 +0800

    KeqingUlt

---
+++ Assets/FangZheng/Scripts/Spell/Spell/AoeSlashes/SlashSumon.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlashSumon : Spell
+{
+    [SerializeField] private GameObject Slash;
+    public override void Attack(SpellCast spellCastList)
+    {
+        SummonUlt(spellCastList);
+    }
+
+    public void SummonUlt(SpellCast spellCastList)
+    {
+        GameObject SpawnedUlt = Instantiate(Slash, transform.position, Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0));
+        SpawnedUlt.GetComponent<SlashUlt>().Init(spellCastList);
+        SpawnedUlt.GetComponent<SlashUlt>().Modify();
+    }
+}
---
+++ Assets/FangZheng/Scripts/Spell/Spell/AoeSlashes/SlashUlt.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SlashUlt : Projectile
+{
+    [SerializeField] private SpellHitbox Hitbox;
+    [SerializeField] private ParticleSystem ParticleSystem;
+
+    private void Start()
+    {
+        if (Hitbox == null)
+        {
+            Hitbox = this.GetComponent<SpellHitbox>();
+        }
+        if (ParticleSystem == null)
+        {
+            ParticleSystem = this.GetComponent<ParticleSystem>();
+        }
+        Hitbox.Initit(spellCast);
+        //Modify();
+    }
+
+    public void Modify()
+    {
+        if (this.GetComponent<SphereCollider>() == null)
+        {
+            this.AddComponent<SphereCollider>();
+        }
+
+        this.GetComponent<SphereCollider>().radius = Radius;
+
+        //if (ParticleSystem != null)
+        //{
+        //    var shape = GetComponent<ParticleSystem>().shape;
+        //    shape.radius = Radius;
+        //}
+
+        //transform.localScale = Vector3.one * Radius * 2;
+        Destroy(this.gameObject, duration);
+    }
+}
